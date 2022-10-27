d536fb567c1f351695b7c9514f1506c216fba2c0
wbrunovieira <wbrunovieira77@gmail.com>
2022-10-27 23:17:04 +0100

    changed to GetStaticProps and format the price

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next"
+import {  GetStaticProps } from "next"
 
 import { HomeContainer, Product } from "../styles/pages/home"
 
@@ -50,7 +50,7 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   })
@@ -60,16 +60,20 @@ export const getServerSideProps: GetServerSideProps = async () => {
     const price = product.default_price as Stripe.Price;
 
     return {
-      id: product.id,
+      
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount! / 100,
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+      }).format(price.unit_amount! / 100),
     }
   })
 
   return {
     props: {
       products
-    }
+    },
+    revalidate: 60 * 60 * 2 // 2 hours,
   }
 }
