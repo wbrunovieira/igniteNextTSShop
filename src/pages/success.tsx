6662521382d2d83277a777c5cf83103c57f05ddc
wbrunovieira <wbrunovieira77@gmail.com>
2022-10-29 09:16:37 +0100

    added verification if there is a sessionId in success to redirect to home

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -34,6 +34,16 @@ export default function Success({ costumerName, product }: SuccessProps) {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+
+  if (!query.session_id) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
+  
   const sessionId = String(query.session_id);
 
   const session = await stripe.checkout.sessions.retrieve(sessionId, {
@@ -41,7 +51,7 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   });
 
   const costumerName = session.customer_details?.name
-  const product = session.line_items.data[0].price.product as Stripe.Product ;
+  const product = session.line_items!.data[0].price!.product! as Stripe.Product ;
 
   return {
     props: {
